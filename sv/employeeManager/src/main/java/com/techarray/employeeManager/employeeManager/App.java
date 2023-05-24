52f4186f77f8c5ccd431ace27c65ff30f2ffa9b8
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-05-24 12:34:04 -0500

    Started the project

---
+++ sv/employeeManager/src/main/java/com/techarray/employeeManager/employeeManager/App.java
@@ -0,0 +1,13 @@
+package com.techarray.employeeManager.employeeManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
