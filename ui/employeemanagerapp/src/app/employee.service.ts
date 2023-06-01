673b904b1e25930bd45a312aec164cf99ca28ce7
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-06-01 16:01:39 -0500

    Implemented service

--- ui/employeemanagerapp/src/app/employee.service.ts
+++ ui/employeemanagerapp/src/app/employee.service.ts
@@ -12,6 +12,18 @@ export class EmployeeService {
     constructor(private http: HttpClient) {}
 
     public getEmployees(): Observable<Employee[]> {
-        return this.http.get<Employee[]>('${this.apiServerUrl}/employee/all');
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    }
+
+    public addEmployees(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`,employee);
+    }
+
+    public updateEmployees(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/add`,employee);
+    }
+
+    public deleteEmployees(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
     }
 }
